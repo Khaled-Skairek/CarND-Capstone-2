a48919e6201cdbc69cc3e15a5457d8c45ef498b1
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2018-04-22 16:08:27 -0700

    update

--- dev/run-docker.sh
+++ dev/run-docker.sh
@@ -6,5 +6,6 @@ PROJECT_HOME="${SCRIPT_DIR}/../"
 docker run -it \
   -v "${PROJECT_HOME}/ros":/capstone/ros \
   -v "${PROJECT_HOME}/data":/capstone/data \
+  -p 4567:4567 \
   --name carnd-capstone \
   carnd-capstone:latest /bin/bash
